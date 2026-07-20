83c5148e5605569297b72b01a05b4227113b5fb1
Manuel Cheta <chetamanuel@gmail.com>
2026-07-20 17:03:41 +0100

    Add exercises folder for scratch drills
    Wire tsconfig/jest to pick up exercises/ so Jest types resolve and tests run there.

---
+++ exercises/scratch.test.ts
@@ -0,0 +1,9 @@
+import { scratch } from "./scratch.1";
+
+describe("scratch", () => {
+  it.each([
+    // [input, expected],
+  ])("scratch(%j) === %j", (input, expected) => {
+    expect(scratch(input)).toEqual(expected);
+  });
+});
